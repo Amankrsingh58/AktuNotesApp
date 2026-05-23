918a0337028706f17ebd59bbca5292b1b60ee01f
Aman Singh <amankrsingh58@gmail.com>
2026-05-23 19:14:52 +0530

    loader done

--- article-client/src/components/CircleDotsPreloader.tsx
+++ article-client/src/components/CircleDotsPreloader.tsx
@@ -13,7 +13,7 @@ export default function CircleDotsPreloader() {
   };
 
   return (
-    <div className="flex items-center justify-center min-h-screen">
+    <div className="fixed inset-0 flex items-center justify-center bg-background/95 backdrop-blur-sm z-50">
       <svg
         width="120"
         height="120"
